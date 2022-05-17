c589f0714457a9e2b277a4c7aea71aff8b519c24
spilbertina <spilbertina@gmail.com>
2022-05-17 13:17:10 +0300

    buttonAll.forEach

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 let button = document.querySelector('.button');
 let display = document.querySelector('.display');
+let buttonAll = document.querySelectorAll('.button');
 
-button.addEventListener('click', () => {
-    display.textContent = button.textContent;
+// button.addEventListener('click', () => {
+//     display.textContent = button.textContent;
+// });
+
+buttonAll.forEach((zuu) => {
+    zuu.addEventListener('click', foo);
 });
+
+function foo(event) {
+    // console.log(event.target.textContent);
+    return display.textContent = event.target.textContent;
+};
+
